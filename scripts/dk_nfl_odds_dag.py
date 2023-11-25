b4afee81841443b14d0a966bae3fb9953cc6adca
Ivanecky <sam.ivanecky3@gmail.com>
2023-11-25 09:30:44 -0600

    initial PySpark setup

---
+++ scripts/dk_nfl_odds_dag.py
@@ -0,0 +1,39 @@
+# Airflow DAG to run DK odds upload
+
+# Import libraries
+import airflow
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+from datetime import timedelta
+import os
+import sys
+
+# Set system path
+sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
+
+# Define the default args
+default_args = {
+    'owner': 'sgi',
+    'start_date': airflow.utils.dates.days_ago(1),
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes = 5)
+}
+
+# Create the DAG
+dag = DAG(
+    'dk_nfl_odds',
+    default_args=default_args,
+    description='DK NFL Odds DAG',
+    schedule_interval = '* * * * *',
+    start_date=airflow.utils.dates.days_ago(1),
+)
+
+# Python operator to run dk_nfl_odds.py
+run_odds_scraper = BashOperator(
+    task_id = 'nfl_odds_scrape',
+    bash_command = 'python3.9 /Users/samivanecky/airflow/scripts/dk_nfl_odds.py',
+    dag = dag
+)
+
---
+++ scripts/dk_s3_transform.py
@@ -0,0 +1,64 @@
+# Import libraries
+import pandas as pd
+import boto3
+from botocore.exceptions import NoCredentialsError 
+import os
+from datetime import datetime as dt
+import yaml 
+from pyspark.sql import SparkSession
+
+# Read in YAML for AWS creds
+# Specify the path to your YAML file
+yaml_fp = '/Users/samivanecky/git/draftkings/secrets.yaml'
+
+# Read the YAML file
+with open(yaml_fp, 'r') as file:
+    creds = yaml.safe_load(file)
+
+# Connect to S3 
+s3 = boto3.resource(
+    service_name='s3',
+    region_name=creds['region_name'],
+    aws_access_key_id=creds['aws_access_key_id'],
+    aws_secret_access_key=creds['aws_secret_access_key']
+)
+
+# Create Spark session
+spark = SparkSession \
+    .builder \
+    .appName("s3 connect") \
+    .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.3.2') \
+    .config('fs.s3a.aws.credentials.provider','org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider') \
+    .config("spark.hadoop.fs.s3a.access.key", creds['aws_access_key_id']) \
+    .config("spark.hadoop.fs.s3a.secret.key", creds['aws_secret_access_key']) \
+    .getOrCreate()
+
+df = spark \
+    .read \
+    .format("csv") \
+    .option("header", True) \
+    .load("s3a://sgi-dk/data_2023-11-12_07-59-26.csv")
+
+# List objects in S3
+objs = list(s3.Bucket('sgi-dk').objects.all())
+
+# Define list for objects to be deleted once they've been read into a dataframe
+objs_to_del = []
+
+# Iterate over objects and read into dataframe
+for o in objs:
+    print(f"Getting data for : {o.key}")
+    # Read file into spark
+    tmp_df = spark \
+        .read \
+        .format("csv") \
+        .option("header", True) \
+        .load(f"s3a://sgi-dk/{o.key}") \
+        .drop('_c0')
+    # Check if overall data exists and union
+    try:
+        df = df.union(tmp_df)
+    except:
+        df = tmp_df
+    # Append name to objects to delete
+    # objs_to_del = objs_to_del.append(str(o.key))
